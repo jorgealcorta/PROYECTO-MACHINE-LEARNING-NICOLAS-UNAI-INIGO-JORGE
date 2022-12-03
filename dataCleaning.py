0069dd7ad964f6a95b5b5400508ecd2eab19f9e9
nicllop <nicllop@opendeusto.es>
2022-12-03 21:53:33 +0100

    asdxasddas

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,33 +1,48 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-def clean_data(df):
-    df2 = df.copy()
-    longitd = len(df2)
-    i = 0
-    while i < longitd:
-        avg_pop_index = i
-        j=i+1
-        while j < longitd:
-            if df2["album_name"][avg_pop_index] == df2["album_name"][j]:
-                if df2["avg_popularity"][avg_pop_index] > df2["avg_popularity"][j]:
-                    df2.drop(j, inplace=True)
-                    df2 = df2.reset_index(drop=True)
-                    longitd = longitd - 1
-                else:
-                    avg_pop_index = j
-                    df2.drop(avg_pop_index, inplace=True)
-                    df2 = df2.reset_index(drop=True)
-                    longitd = longitd - 1         
-            j = j + 1   
-        i = i + 1
-    return df2
+
+
+       
+def filter_mutual_info(df, target_column, k_num):
+ 
+   
+   y = df[target_column]
+   X = df.drop(columns=target_column )
+
+    
+   selector = SelectKBest(mutual_info_classif, k=k_num)
+   X_reduced = selector.fit_transform(X, y)
+   print(X_reduced.shape)
+
+   return None         
                 
+def std_scaler(feature_df:pd.DataFrame, target_column:str= "popularity"):
+   scaler = StandardScaler()
+   scaler.fit(drop_columns_if_exist(feature_df, [target_column]).values)
+   return scaler
 
-            
-df = pd.read_csv("first_scrape_500.csv")
-df = clean_data(df)
-df.to_csv("first_scrape_500_cleaned.csv")
-print(df["album_name"][39])
-print(df["album_name"][323])
-print(df["album_name"][39] == df["album_name"][323])
+def object_column_to_categorical(feature_df: pd.DataFrame, column:str, set_of_values:list=None):
+    df = feature_df.copy(deep=True)
+    df[column] = df[column].astype(str)
+
+    if set_of_values is None:
+        set_of_values = set(df[column].values)
+
+    value_columns = [column + "_is_" + str(value) for value in set_of_values]
+    df = drop_columns_if_exist(df, value_columns)
+
+    loc_column = df.columns.get_loc(column)
+
+    for value in set_of_values:
+        loc_column += 1
+        categorical_values = (df[column] == value) * 1.0
+        df.insert(loc=loc_column, column=column + "_is_" + str(value), value=categorical_values)
+    
+    df = drop_columns_if_exist(df, [column])
+    return df
+
+def preprocess (df):
+
+   return void
---
+++ project.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+import numppy as np
+from dataCleaning import *
+from regression_models import *
+
+
+column_names = {"song_id", "song_name", "artist_name", "duration_ms", "danceability", "energy", "key", "loudness", "mode", "loudness", "mode", "speechiness", "acousticness", 
+                "instrumentalness", "liveness", "valence", "tempo", "popularity",  "popularity", "artist_followers", "number_of_artists", "number_of_markets"}
+target_column = {"popularity"}
+
+
+df = pd.read_csv("datasets_kaggle/dataset_unido_anyadidos.csv", sep = ";")
+
+
+
+df = preprocess(df)
+
+scaler = std_scaler(df, target_column = target_column)
+
+(X_train, Y_train), (X_test, Y_test) = split_train_test_df(df, target_column)
+
+model = model_1(X_train.shape[1])
+
+model.fit(X_train, Y_train,
+          batch_size=128,
+          epochs=2,
+          verbose=1,
+          validation_data=(X_test, Y_test)
+)
+
